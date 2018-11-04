fd928b8a758f519724ec1ed802de8d6420081f95
KSiuda <konrad.siuda@gmail.com>
2018-11-04 01:07:22 +0100

    Mini games

---
+++ Warriors/Warriors/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+namespace Warriors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Warrior maximus = new Warrior("Maximus", 1000, 120, 40);
+            Warrior bob = new Warrior("Bob", 1000, 120, 40);
+
+            Battle.StartFight(maximus, bob);
+
+            Console.ReadLine();
+
+        }
+
+
+    }
+}
